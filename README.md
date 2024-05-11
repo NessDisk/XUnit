4baccbd0a91d222987760e554efe294a5dc7b781
Marcel Solera <59302496+NessDisk@users.noreply.github.com>
2024-05-10 19:55:49 -0500

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# XUnit
+## Código base para completar el curso de prueba unitarias en .NET y C# con xUnit
+Debe descargar el codigo y usar el branch master o 0-codigobase
---
+++ StringManipulationTest/StringOperationTest.cs
@@ -0,0 +1,56 @@
+﻿using StringManipulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StringManipulationTest
+{
+    
+    public class StringOperationTest
+    {
+
+        [Fact]
+        public void ConcantenateString()
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+
+            // Act
+            var result = strOperations.ConcatenateStrings("Hello", "Platzi");
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.Equal("Hello Platzi", result);
+        }
+
+        [Fact]
+        public void IsPalindrome_True()
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+
+            // Act
+            var result = strOperations.IsPalindrome("ama");
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsPalindrome_False()
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+
+            // Act
+            var result = strOperations.IsPalindrome("hello");
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
